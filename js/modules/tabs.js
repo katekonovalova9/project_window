ab21147333f9a80d755f2900cc7ce203e2725c95
kate <katekonovalova96k@gmail.com>
2020-05-28 16:57:50 +0300

    add

---
+++ js/modules/tabs.js
@@ -0,0 +1,40 @@
+//инициализация табов 
+const tabs = (headerSelector, tabSelector, contentSelector, activeClass, display='block') => {
+    const header = document.querySelector(headerSelector), //общий элемент
+          tab = document.querySelectorAll(tabSelector), //таб
+          content = document.querySelectorAll(contentSelector);//активный класс
+
+    function hideTabContent(){
+        content.forEach(item => {
+            item.style.display='none';
+        });
+
+        tab.forEach(item => {
+            item.classList.remove(activeClass);
+        });
+    }
+
+    function showTabContent(i=0){
+        content[i].style.display = display;
+        tab[i].classList.add(activeClass);
+    }
+
+    hideTabContent();
+    showTabContent();
+
+    header.addEventListener('click', (e) => {
+        const target = e.target;
+
+        if(target && (target.classList.contains(tabSelector.replace(/\./, '')) // .class => class
+            || target.parentNode.classList.contains(tabSelector.replace(/\./, '')))){ // ловим клик на элементе
+                tab.forEach((item, i) => {   // проверяем таб какой
+                    if(target == item || target.parentNode == item){
+                        hideTabContent();
+                        showTabContent(i);
+                    }
+                });
+        }
+    });
+};
+
+export default tabs;
